2766ae37d043fe0c9c0fc524af3223d518428129
banana <fabio.maia@ua.pt>
2015-07-03 03:12:00 +0100

    progressively add text clipping effect to webkit

--- src/_assets/scripts/header.js
+++ src/_assets/scripts/header.js
@@ -9,17 +9,24 @@
         return array[randomIntBetween(0, array.length-1)];
     };
 
+    // Return true if webkit based browser
+    var isWebKit = 'WebkitAppearance' in document.documentElement.style;
+
     // List of header images
     var images = ['header2.jpg', 'header3.jpeg', 'header5.jpg', 'header6.jpg'];
 
     // Query the DOM for the elements
     var header = document.querySelector('.header');
-    var logo = document.querySelector('.header__logo span');
+    var headerLogo = document.querySelector('.header__logo');
+    var headerLogoSpan = document.querySelector('.header__logo span');
 
     // Calculate the random image to be loaded
     var randomImageFilename = randomArrayElement(images);
 
     // Set an initial random image
     header.style.backgroundImage = "url('../assets/images/" + randomImageFilename + "')";
-    logo.style.backgroundImage = "url('../assets/images/" + randomImageFilename + "')";
+    if(isWebKit) {
+        headerLogo.style.backgroundColor = '#fff';
+        headerLogoSpan.style.backgroundImage = "url('../assets/images/" + randomImageFilename + "')";
+    }
 })();
