69357987d0590cde82c61cf8e2b451adc87059ab
Fábio Maia <fabio.maia@bliq.ai>
2021-05-10 18:35:04 +0000

    Refactor

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,11 @@
-#!/bin/sh -ex
+#!/bin/sh
+set -ex
 
 #docker run -it --rm -v ${PWD}:/tmp validator/validator vnu --Werror --also-check-css --also-check-svg --verbose /tmp/index.html
 #docker run -it --rm -v ${PWD}:/tmp linkchecker/linkchecker --verbose --check-extern /tmp/index.html
 #docker run -it --rm -v ${PWD}:/tmp flungo/html-minify sh -c "mkdir /tmp/out && html-minifier --collapse-whitespace --remove-comments --remove-optional-tags --remove-redundant-attributes --remove-script-type-attributes --remove-tag-whitespace --use-short-doctype --minify-css true --minify-js true /tmp/index.html > /tmp/out/index.html"
 
 docker run -d -p 80:80 -p 443:443 \
-	-v $(pwd):/srv \
-	-v $(pwd)/caddy_data:/data \
-	-v $(pwd)/caddy_config:/config \
-	caddy caddy file-server --domain fabiomaia.eu
+	-v "$PWD:/srv" \
+	-v "$PWD/Caddyfile:/etc/caddy/Caddyfile" \
+	caddy caddy run
